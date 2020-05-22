027bd24976e21d235a391870708250dc5fded584
Zach Duguid <zach.duguid@gmail.com>
2020-05-22 15:48:05 -0400

    Implement flight controller class
    Flight controller class (SlocumFlightController) was created to
    parse flight controller files in .asc file format. A subset of
    the variables are extracted and saved in time series format.
    
    Also, cleaned up the dvl_plotter jupyter notebook by creating a
    separate plotting function instead of storing all plotting commands
    in the notebook itself.

---
+++ SlocumFlightController.py
@@ -0,0 +1,239 @@
+# SlocumFlightController.py
+#
+# Class for parsing .dbd files 
+#   2020-05-22  zduguid@mit.edu         initial implementation
+
+import pandas as pd
+import numpy as np
+import time 
+from datetime import datetime
+
+
+class SlocumFlightController(object):
+    def __init__(self, name=datetime.now().strftime("%Y-%m-%d %H:%M:%S")):
+        """Represents a Slocum Glider flight log. 
+        
+        Note that only a subset of the variables are extracted. If more 
+        variables are desired, simply add them to the label list below.
+        """
+        self._label_list = [
+            'm_present_time',
+            'm_speed',
+            'm_pitch',
+            'c_pitch',
+            'm_roll',
+            'c_roll',
+            'm_heading',
+            'c_heading',
+            'm_fin',
+            'c_fin',
+            'm_battery',
+            'm_vacuum',
+            'm_depth',
+            'm_pressure',
+            'm_altitude',
+            'm_water_depth',
+            'm_depth_rate_avg_final',
+            'm_final_water_vx',
+            'm_final_water_vy',
+            'm_water_vx',
+            'm_water_vy',
+            'c_wpt_lat',
+            'c_wpt_lon',
+            'm_vx_lmc',
+            'm_vy_lmc',
+            'm_lat',
+            'm_lon',
+            'm_dr_time',
+            'm_ext_x_lmc',
+            'm_ext_y_lmc',
+            'm_ext_z_lmc',
+            'm_dr_surf_x_lmc',
+            'm_dr_surf_y_lmc',
+            'm_x_lmc',
+            'm_y_lmc',
+            'x_lmc_xy_source',
+            'm_gps_lat',
+            'm_gps_lon',
+            'm_gps_fix_x_lmc',
+            'm_gps_fix_y_lmc',
+            'm_gps_x_lmc',
+            'm_gps_y_lmc',
+            'm_gps_status',
+            'm_gps_full_status',
+            'm_appear_to_be_at_surface',
+            'sci_m_present_time',
+            'm_science_clothesline_lag',
+            'x_software_ver'
+        ]
+        self._header        = None
+        self._df            = None
+        self._label_set     = set(self.label_list)
+        self._ensemble_size = len(self.label_list)
+        self._data_lookup   = {self.label_list[i]:i 
+            for i in range(self.ensemble_size)}
+        self._ensemble_list = []
+
+
+    @property
+    def label_list(self):
+        return self._label_list
+
+    @property
+    def label_set(self):
+        return self._label_set
+    
+    @property
+    def ensemble_size(self):
+        return self._ensemble_size
+
+    @property
+    def data_lookup(self):
+        return self._data_lookup
+
+    @property
+    def header(self):
+        return self._header
+
+    @property
+    def df(self):
+        return self._df
+
+    @property
+    def var_names(self):
+        return self._var_names
+
+    @property
+    def var_units(self):
+        return self._var_units
+
+    @property
+    def var_sizes(self):
+        return self._var_sizes
+
+    @property
+    def var_dict(self):
+        return self._var_dict
+
+    @property
+    def ensemble_list(self):
+        return self._ensemble_list
+
+
+    def add_ensemble(self, ensemble):
+        """Adds an ensemble to the ensemble list.
+        """
+        self._ensemble_list.append(ensemble)    
+
+
+    def to_dataframe(self):
+        """Converts the current list of ensembles into a DataFrame.
+
+        Note: calling this function will invoke pd.concat(), which creates a 
+        copy of the whole DataFrame in memory. As a result, if this function 
+        is called many times, there will be significant slowdown. Instead,
+        consider collecting ensembles into the ensemble_list until a suitable 
+        number of ensembles have been collected, and then intermittently call 
+        the to_dataframe function.
+        """
+        # convert available ensembles to DataFrame
+        if self.ensemble_list:
+            ts      = np.array(self.ensemble_list)
+            cols    = self.label_list
+            t_index = self.data_lookup['m_present_time']
+            t       = ts[:,t_index]
+            index   = pd.DatetimeIndex([datetime.fromtimestamp(v) for v in t])
+            new_df  = pd.DataFrame(data=ts, index=index, columns=cols)
+
+            # concatenate new ensembles with existing DataFrame if possible
+            if self.df is None:
+                self._df = new_df
+            else:
+                self._df = pd.concat([self.df, new_df])
+
+            # reset the ensemble list once added to the DataFrame
+            self._ensemble_list = []
+        else:
+            print("WARNING: No ensembles to add to DataFrame.")
+    
+    
+    @classmethod
+    def from_asc(cls, filepath, save, verbose=True):
+        """Parses DBD (flight controller file) from the Slocum Glider.
+
+        Args: 
+            filepath: the file location of the pd0 to be parsed, requires that 
+                file located at filepath is a valid pd0 file
+            save: boolean flag for saving the resulting time-series or not
+            verbose: boolean flag for printing file information while parsing
+        """
+        PRINT_INTERVAL = 200 
+        HEADER_LEN = 14
+
+        # open the file 
+        asc_file = open(filepath, 'rb').read()
+        filename = filepath.split('/')[-1]
+        count = 0
+        if verbose:
+            print('________________________________________')
+            print('- Parsing Slocum File (Flight) ---------')
+            print('    input file: %s' % (filename,))
+            parse_start = time.time()
+
+        # initialize the time series object
+        name   = filepath.split('/')[-1].split('.')[0]
+        ts     = cls(name)
+        header = {}
+
+        # parse the ASC file 
+        with open(filepath, 'r') as f:
+            # parse the fixed file header in formation 
+            for i in range(HEADER_LEN):
+                head_line = f.readline().split(': ')
+                header[head_line[0]] = head_line[1].split('\n')[0]
+            ts._header = header
+
+            # parse the variables names, sizes
+            ts._var_names = f.readline().split(' ')[:-1]
+            ts._var_units = f.readline().split(' ')[:-1]
+            ts._var_sizes = f.readline().split(' ')[:-1]
+            ts._var_dict  = {_ : ts.var_names.index(_) for _ in ts.label_list}
+
+            # parse ensembles until the file is empty 
+            line   = [float(_) for _ in f.readline().split(' ')[:-1]]
+            while line:
+                count += 1
+                ens = np.array([line[ts.var_dict[_]] for _ in ts.label_list])
+                ts.add_ensemble(ens)
+                line = [float(_) for _ in f.readline().split(' ')[:-1]]
+
+                # print number of ensembles parsed periodically 
+                if verbose:
+                    if (count % PRINT_INTERVAL == 0):
+                        print('    # ensembles:  %5d' % (count,))
+
+        # parsing completed 
+        ts.to_dataframe()
+        if verbose:
+            parse_stop = time.time()
+            print('- Parsing Complete ---------------------')
+            print('    # ensembles:  %5d'    % (count))
+            print('    parsing time:  %f'    % (parse_stop - parse_start))
+
+        # save the file to .csv format
+        if save:
+            
+            # determine how to save the file
+            root, _, glider, _ = filepath.rsplit('/',3)
+            directory = root + '/' + 'dbd-parsed/' + glider + '/'
+            
+            # save the file using pandas 
+            save_start = time.time()
+            time_series.save_as_csv(name=name, directory=directory)
+            save_stop  = time.time()
+            if verbose:
+                print('    saving time:   %f'    % (save_stop - save_start))
+                print('    output file:   %s'    % (name+'.CSV'))
+     
+        return(ts)
+
---
+++ dvl_plotter.py
@@ -0,0 +1,434 @@
+# micron_plotter.py
+# 
+# Plotting utilities for Micron Sonar
+#   2020-05-22  zduguid@mit.edu         initial implementation  
+
+import math
+import datetime
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
+
+unit_name = {"sentinel" : "Unit 250",
+             "unit_770" : "Unit 770" }
+
+
+###############################################################################
+# PLOT PROFILE
+###############################################################################
+def plot_profile(ts, glider, save_name=None):
+    # pitch data
+    sns.set(font_scale = 1.5)
+    pitch = ts.df['pitch']
+    line_plot = pitch.plot(figsize=(15,8), linewidth=3, color='tab:green')
+
+    # depth data
+    depth     = -1 * ts.df['depth']
+    line_plot = depth.plot(figsize=(15,8), linewidth=3, color='tab:orange')
+
+    # compute altitude estimate from the four vertical range estimates
+    # - does not account for pitch and roll of the vehicle 
+    h1 = ts.df['btm_beam0_range']
+    h2 = ts.df['btm_beam1_range']
+    h3 = ts.df['btm_beam2_range']
+    h4 = ts.df['btm_beam3_range']
+    altitude = depth - ((h1*h2)/(h1 + h2) + (h3*h4)/(h3 + h4))
+    altitude.plot(linewidth=3, color='tab:blue', zorder=1)
+
+    # bottom_track slant range data 
+    bt_ranges = [
+        'btm_beam0_range',
+        'btm_beam1_range',
+        'btm_beam2_range',
+        'btm_beam3_range'
+    ]
+    bt_colors = ['powderblue','darkturquoise','lightsteelblue','deepskyblue']
+    for i in range(len(bt_ranges)):
+        bt_range  = depth - ts.df[bt_ranges[i]]
+        bt_range.plot(linewidth=1, color=bt_colors[i], zorder=0)
+
+    # plot moments in time where the glider gets dangerously close to bottom 
+    window = 5
+    danger = 20
+    danger = ts.df[(ts.df.btm_beam0_range < danger) & 
+                    (ts.df.btm_beam1_range < danger) & 
+                    (ts.df.btm_beam2_range < danger) & 
+                    (ts.df.btm_beam3_range < danger)]
+    for time_stamp in danger.index:
+        plt.axvspan(time_stamp, time_stamp + pd.Timedelta(seconds=window), 
+            color='tab:red', alpha=0.05)
+            
+    # plotting labels 
+    dt = datetime.datetime.fromtimestamp(ts.df.time[0]).replace(microsecond=0)
+    plt.legend(['Pitch [deg]', 'Depth [m]', 'Altitude [m]', 
+        'Vertical Ranges [m]'], fontsize='small', loc='lower left',
+        framealpha=1)
+    plt.suptitle('Deployment Profile', fontweight='bold')
+    plt.title('%s Kolumbo Volcano %s' % (unit_name[glider], dt.isoformat(),))
+    plt.ylabel('Depth [m]')
+    plt.xlabel('Time')
+    if save_name: plt.savefig(save_name)
+    else:         plt.savefig('/Users/zduguid/Desktop/fig/tmp.png')
+
+
+###############################################################################
+# PLOT ODOMETRY
+###############################################################################
+def plot_odometry(ts, glider, save_name=None):
+    sns.set(font_scale = 1.5)
+    fig, ax = plt.subplots(figsize=(10,8))
+    sns.scatterplot(
+        x=ts.df.rel_pos_x,
+        y=ts.df.rel_pos_y,
+        palette='viridis_r',
+        hue=ts.df.depth,
+        linewidth=0,
+        s=10,
+        data=ts.df)
+    dt = datetime.datetime.fromtimestamp(ts.df.time[0]).replace(microsecond=0)
+    plt.axis('equal')
+    plt.suptitle('DVL Odometry', fontweight='bold')
+    plt.title('%s Kolumbo Volcano %s' % (unit_name[glider], dt.isoformat(),))
+    plt.xlabel('x position [m]')
+    plt.ylabel('y position [m]')
+    if save_name: plt.savefig(save_name)
+    else:         plt.savefig('/Users/zduguid/Desktop/fig/tmp.png')
+
+
+###############################################################################
+# PLOT ODOMETRY (DEAD-RECKONED)
+###############################################################################
+def plot_m_odometry_dr(ts_g, glider, save_name=None):
+    sns.set(font_scale = 1.5)
+    fig, ax = plt.subplots(figsize=(10,8))
+    sns.scatterplot(
+        ts_g.df.m_gps_fix_x_lmc, 
+        ts_g.df.m_gps_fix_y_lmc, 
+        marker='X',
+        color='tab:red', 
+        s=300
+    )
+    sns.scatterplot(
+        x=ts_g.df.m_x_lmc,
+        y=ts_g.df.m_y_lmc,
+        palette='viridis_r',
+        hue=ts_g.df.m_depth,
+        linewidth=0,
+        s=10,
+        data=ts_g.df
+    )
+    dt = datetime.datetime.fromtimestamp(
+        ts_g.df.m_present_time[0]).replace(microsecond=0)
+    plt.axis('equal')
+    plt.suptitle('Dead Reckoned Trajectory', fontweight='bold')
+    plt.title('%s Kolumbo Volcano %s' % (unit_name[glider], dt.isoformat(),))
+    plt.xlabel('x position [m]')
+    plt.ylabel('y position [m]')
+    if save_name: plt.savefig(save_name)
+    else:         plt.savefig('/Users/zduguid/Desktop/fig/tmp.png')
+
+
+###############################################################################
+# PLOT PROFILE AND ODOMETRY
+###############################################################################
+def plot_profile_and_odometry(ts, glider, save_name=None):
+    sns.set(font_scale = 1.5)
+    fig, ax = plt.subplots(1,2, figsize=(15,8))
+
+    # profile
+    depth = -1 * ts.df['depth']
+    line_plot = depth.plot(figsize=(15,8), linewidth=3, color='tab:orange', ax=ax[0])
+
+    # compute altitude estimate from the four vertical range estimates
+    # - does not account for pitch and roll of the vehicle 
+    h1 = ts.df['btm_beam0_range']
+    h2 = ts.df['btm_beam1_range']
+    h3 = ts.df['btm_beam2_range']
+    h4 = ts.df['btm_beam3_range']
+    altitude = depth - ((h1*h2)/(h1 + h2) + (h3*h4)/(h3 + h4))
+    altitude.plot(linewidth=3, color='tab:blue', zorder=1, ax=ax[0])
+
+    # bottom_track slant range data 
+    bt_ranges = [
+        'btm_beam0_range',
+        'btm_beam1_range',
+        'btm_beam2_range',
+        'btm_beam3_range'
+    ]
+    bt_colors = ['powderblue','darkturquoise','lightsteelblue','deepskyblue']
+    for i in range(len(bt_ranges)):
+        bt_range  = depth - ts.df[bt_ranges[i]]
+        bt_range.plot(linewidth=1, color=bt_colors[i], zorder=0, ax=ax[0])
+    ax[0].set_ylabel('depth [m]')
+    ax[0].set_xlabel('time')
+    ax[0].set_title('Dive Profile')
+    ax[0].legend(['Depth [m]', 'Altitude [m]'], fontsize='small', 
+        loc='lower left', framealpha=0.5)
+
+    # odometry
+    sns.scatterplot(
+        x=ts.df.rel_pos_x,
+        y=ts.df.rel_pos_y,
+        palette='viridis_r',
+        hue=ts.df.depth,
+        linewidth=0,
+        s=10,
+        data=ts.df,
+        ax=ax[1])
+    dt = datetime.datetime.fromtimestamp(ts.df.time[0]).replace(microsecond=0)
+    plt.axis('equal')
+    plt.suptitle('%s Kolumbo Volcano %s' % (unit_name[glider], dt.isoformat(),), fontweight='bold')
+    plt.title('DVL Odometry')
+    plt.xlabel('x position [m]')
+    plt.ylabel('y position [m]')
+    plt.legend(loc='lower right')
+    if save_name: plt.savefig(save_name)
+    else:         plt.savefig('/Users/zduguid/Desktop/fig/tmp.png')
+
+
+###############################################################################
+# PLOT VELOCITIES (BOTTOM TRACK)
+###############################################################################
+def plot_velocity_bottom_track(ts, glider, save_name=None):
+    sns.set(font_scale = 1.5)
+    fig, ax = plt.subplots(figsize=(10,8))
+    sns.scatterplot(x=ts.df.abs_vel_btm_u,
+                    y=ts.df.abs_vel_btm_v,
+                    s=30,
+                    hue=ts.df.heading,
+                    palette='viridis_r',
+                    data=ts.df)
+    dt = datetime.datetime.fromtimestamp(ts.df.time[0]).replace(microsecond=0)
+    plt.axis('equal')
+    plt.suptitle('Bottom Track Velocities', fontweight='bold')
+    plt.title('%s Kolumbo Volcano %s' % (unit_name[glider], dt.isoformat(),))
+    plt.xlabel('East Velocity [m/s]')
+    plt.ylabel('North Velocity [m/s]')
+    tick_spacing = np.arange(-1.2,1.4,0.2)
+    plt.xticks(tick_spacing)
+    plt.yticks(tick_spacing)
+    if save_name: plt.savefig(save_name)
+    else:         plt.savefig('/Users/zduguid/Desktop/fig/tmp.png')
+
+
+###############################################################################
+# PLOT VELOCITIES (EASTWARD)
+###############################################################################
+def plot_velocity_eastward(ts, glider, save_name=None):
+    sns.set(font_scale = 1.5)
+    fig, ax = plt.subplots(figsize=(15,8))
+    filter_len = 30
+    sns.scatterplot(
+        x=ts.df.time,
+        y=-ts.df.vel_bin0_beam0.rolling(filter_len).mean(),
+        color='lightblue',
+        data=ts.df,
+        s=10,
+        linewidth=0,
+        label='bin 0'
+    )
+    sns.scatterplot(
+        x=ts.df.time,
+        y=-ts.df.vel_bin1_beam0.rolling(filter_len).mean(),
+        color='deepskyblue',
+        data=ts.df,
+        s=10,
+        linewidth=0,
+        label='bin 1'
+    )
+    sns.scatterplot(
+        x=ts.df.time,
+        y=-ts.df.vel_bin2_beam0.rolling(filter_len).mean(),
+        color='cornflowerblue',
+        data=ts.df,
+        s=10,
+        linewidth=0,
+        label='bin 2'
+    )
+    sns.scatterplot(
+        x=ts.df.time,
+        y=-ts.df.vel_bin3_beam0.rolling(filter_len).mean(),
+        color='royalblue',
+        data=ts.df,
+        s=10,
+        linewidth=0,
+        label='bin 3'
+    )
+    sns.scatterplot(
+        x=ts.df.time,
+        y=ts.df.abs_vel_btm_u.rolling(filter_len).mean(),
+        color='tab:orange',
+        data=ts.df,
+        s=10,
+        linewidth=0,
+        label='btm'
+    )
+    sns.scatterplot(
+        x=ts.df.time,
+        y=ts.df.rel_vel_pressure_u.rolling(filter_len).mean(),
+        color='magenta',
+        data=ts.df,
+        s=10,
+        linewidth=0,
+        label='$\Delta$z/$\Delta$t'
+    )
+    sns.scatterplot(
+        x=ts.df.time,
+        y=ts.df.pitch.rolling(filter_len).median()/100,
+        color='red',
+        data=ts.df,
+        s=10,
+        linewidth=0,
+        label='pitch'
+    )
+    dt = datetime.datetime.fromtimestamp(ts.df.time[0]).replace(microsecond=0)
+    plt.suptitle('Eastward Component of Velocity', fontweight='bold')
+    plt.title('%s Kolumbo Volcano %s' % (unit_name[glider], dt.isoformat(),))
+    plt.xlabel('Time')
+    plt.ylabel('Velocity [m/s]')
+    if save_name: plt.savefig(save_name)
+    else:         plt.savefig('/Users/zduguid/Desktop/fig/tmp.png')
+
+
+###############################################################################
+# PLOT VELOCITIES (NORTHWARD) 
+###############################################################################
+def plot_velocity_northward(ts, glider, save_name=None):
+    sns.set(font_scale = 1.5)
+    fig, ax = plt.subplots(figsize=(15,8))
+    sns.scatterplot(
+        x=ts.df.time,
+        y=-ts.df.vel_bin0_beam1.rolling(10).median(),
+        color='lightblue',
+        data=ts.df,
+        s=10,
+        linewidth=0,
+        label='bin 0'
+    )
+    sns.scatterplot(
+        x=ts.df.time,
+        y=-ts.df.vel_bin1_beam1.rolling(10).median(),
+        color='deepskyblue',
+        data=ts.df,
+        s=10,
+        linewidth=0,
+        label='bin 1'
+    )
+    sns.scatterplot(
+        x=ts.df.time,
+        y=-ts.df.vel_bin2_beam1.rolling(10).median(),
+        color='cornflowerblue',
+        data=ts.df,
+        s=10,
+        linewidth=0,
+        label='bin 2'
+    )
+    sns.scatterplot(
+        x=ts.df.time,
+        y=-ts.df.vel_bin3_beam1.rolling(10).median(),
+        color='royalblue',
+        data=ts.df,
+        s=10,
+        linewidth=0,
+        label='bin 3'
+    )
+    sns.scatterplot(
+        x=ts.df.time,
+        y=-ts.df.vel_bin4_beam1.rolling(10).median(),
+        color='blue',
+        data=ts.df,
+        s=10,
+        linewidth=0,
+        label='bin 4'
+    )
+    sns.scatterplot(
+        x=ts.df.time,
+        y=-ts.df.vel_bin5_beam1.rolling(10).median(),
+        color='darkblue',
+        data=ts.df,
+        s=10,
+        linewidth=0,
+        label='bin 5'
+    )
+    sns.scatterplot(
+        x=ts.df.time,
+        y=ts.df.abs_vel_btm_v.rolling(10).median(),
+        color='tab:orange',
+        data=ts.df,
+        s=10,
+        linewidth=0,
+        label='btm'
+    )
+    sns.scatterplot(
+        x=ts.df.time,
+        y=ts.df.rel_vel_pressure_v.rolling(10).median(),
+        color='magenta',
+        data=ts.df,
+        s=10,
+        linewidth=0,
+        label='$\Delta$z/$\Delta$t'
+    )
+    dt = datetime.datetime.fromtimestamp(ts.df.time[0]).replace(microsecond=0)
+    plt.suptitle('Northward Component of Velocity', fontweight='bold')
+    plt.title('%s Kolumbo Volcano %s' % (unit_name[glider], dt.isoformat(),))
+    plt.xlabel('Time')
+    plt.ylabel('Velocity [m/s]')
+    plt.savefig('/Users/zduguid/Desktop/fig/tmp.png')
+    if save_name: plt.savefig(save_name)
+    else:         plt.savefig('/Users/zduguid/Desktop/fig/tmp.png')
+
+
+###############################################################################
+# PLOT CORRELATIONS
+###############################################################################
+def plot_correlations(ts, glider, save_name=None):
+    cols = [
+        'pitch', 
+        'heading', 
+        'roll', 
+        'depth',
+        'temperature',
+        'speed_of_sound',
+        'abs_vel_btm_u',
+        'abs_vel_btm_v',
+        'abs_vel_btm_w',
+        'rel_vel_dvl_u',
+        'rel_vel_dvl_v',
+        'rel_vel_dvl_w',
+        'rel_vel_pressure_u',
+        'rel_vel_pressure_v',
+        'rel_vel_pressure_w',
+        'rel_pos_x',
+        'rel_pos_y',
+        'rel_pos_z',
+    ]
+
+    df = ts.df[cols]
+
+    # compute correlations 
+    corr = df.corr()
+    mask = np.zeros_like(corr, dtype=np.bool)
+    mask[np.triu_indices_from(mask)] = True
+    mask[np.diag_indices_from(mask)] = False
+
+    # plot heatmap given the correlations 
+    fig, (ax) = plt.subplots(1, 1, figsize=(15,8))
+    hm = sns.heatmap(
+        corr, 
+        ax=ax,
+        #mask=mask,
+        cmap="coolwarm",
+        #square=True,
+        annot=True, 
+        fmt='.2f', 
+        #annot_kws={"size": 14},
+        linewidths=.05
+    )
+
+    # fig.subplots_adjust(top=0.93)
+    ax.set_title('DVL Feature Correlations', fontsize=22, fontweight='bold')
+    if save_name: plt.savefig(save_name)
+    else:         plt.savefig('/Users/zduguid/Desktop/fig/tmp.png')
